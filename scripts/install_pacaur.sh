51b74df30d68489797217e858264170267968f83
Mark Wright <thepiercingarrow@gmail.com>
2016-08-17 01:48:05 -0400

    Create install_pacaur.sh

---
+++ scripts/install_pacaur.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+pacman -S expac yajl git --noconfirm
+mkdir pacaur/
+cd pacaur/
+curl -o PKGBUILD https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=cower
+makepkg PKGBUILD --skippgpcheck
+sudo pacman -U cower*.tar.xz --noconfirm
+curl -o PKGBUILD https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=pacaur
+makepkg PKGBUILD
+sudo pacman -U pacaur*.tar.xz --noconfirm
+cd ..
+rm -rf pacaur/
